716138f08a9040f1195a34fba2d507c5c7ba4164
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-06-06 15:44:52 +0900

    add: User_Dataのmain.goから呼び出すcontroller部分の追加

---
+++ go/app/controller/User.go
@@ -0,0 +1,14 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetUserHandler(c *gin.Context) {
+}
+
+func AddUserHandler(c *gin.Context) {
+}
+
+func EditUserHandler(c *gin.Context) {
+}
--- go/main.go
+++ go/main.go
@@ -12,6 +12,8 @@ func main() {
 	versionEngine := r.Group("/api")
 	{
 		versionEngine.GET("/users", controller.GetUserHandler)
+		versionEngine.POST("/users", controller.AddUserHandler)
+		versionEngine.PUT("/users", controller.EditUserHandler)
 		versionEngine.GET("/weight/:userId", controller.GetWeightHandler)
 		versionEngine.PUT("/weight/:userId", controller.UpdateWeightHandler)
 	}
