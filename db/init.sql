eac26b6c3daf6c70d192768d108cf44c1974db9d
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-06-12 17:03:25 +0900

    fix: 初期DBテーブル設定の変更

--- db/init.sql
+++ db/init.sql
@@ -4,19 +4,28 @@ CREATE TABLE User_Data (
     deleted BOOLEAN DEFAULT FALSE
 )CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE Weight_Data (
+CREATE TABLE Model_Data (
+    model_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
-    current_weight FLOAT,
     model_weight FLOAT,
-    length_of_month INT,
-    calorie INT,
+    length_of_month FLOAT,
+    created_date DATETIME,
     FOREIGN KEY (user_id) REFERENCES User_Data(user_id)
 );
 
-CREATE TABLE Calorie_Data (
-    user_id INT,
-    model_calorie INT,
-    current_calorie INT,
+CREATE TABLE Weight_Data (
+    weight_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    model_id INT,
+    current_weight FLOAT,
     created_date DATETIME,
-    FOREIGN KEY (user_id) REFERENCES User_Data(user_id)
+    FOREIGN KEY (model_id) REFERENCES Model_Data(model_id)
+);
+
+CREATE TABLE Calorie_Data (
+    calorie_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    weight_id INT,
+    must_calorie FLOAT,
+    current_calorie FLOAT,
+    created_date DATETIME,
+    FOREIGN KEY (weight_id) REFERENCES Weight_Data(weight_id)
 );
