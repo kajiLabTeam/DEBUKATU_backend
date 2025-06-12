7761ca2664290f7e6d567f7993901966b6bb4592
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-06-12 17:25:55 +0900

    add: Model_Dataに代入するデータの型定義

---
+++ app/model/model.go
@@ -0,0 +1,13 @@
+package model
+
+import "time"
+
+type Model struct {
+	ModelId       int64     `gorm:"column:model_id;primaryKey;autoIncrement"`
+	UserId        int64     `gorm:"column:user_id"`
+	ModelWeight   float64   `gorm:"column:model_weight"`
+	LengthOfMonth float64   `gorm:"column:length_of_month"`
+	CreatedDate   time.Time `gorm:"column:created_date"`
+}
+
+func (Model) TableName() string { return "Model_Data" }
