97c3456124e3b108700510119648f8b0b0f77132
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-06-13 17:05:12 +0900

    fix: DB構造の変更

--- db/init.sql
+++ db/init.sql
@@ -8,7 +8,7 @@ CREATE TABLE Model_Data (
     model_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     model_weight FLOAT,
-    length_of_days FLOAT,
+    length_of_days INT,
     created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES User_Data(user_id)
 );
@@ -16,16 +16,20 @@ CREATE TABLE Model_Data (
 CREATE TABLE Weight_Data (
     weight_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     model_id INT,
+    user_id INT,
     current_weight FLOAT,
     created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (model_id) REFERENCES Model_Data(model_id)
+    FOREIGN KEY (user_id) REFERENCES User_Data(user_id)
 );
 
 CREATE TABLE Calorie_Data (
     calorie_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     weight_id INT,
-    must_calorie FLOAT,
-    current_calorie FLOAT,
+    model_id INT,
+    must_calorie INT,
+    current_calorie INT,
     created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (weight_id) REFERENCES Weight_Data(weight_id)
+    FOREIGN KEY (model_id) REFERENCES Model_Data(model_id)
 );
