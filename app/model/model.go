b0ce3384f8b567e6526e357edda79f48446cd35a
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-06-13 10:57:37 +0900

    add: 型定義を明確に

--- app/model/model.go
+++ app/model/model.go
@@ -7,7 +7,7 @@ type Model struct {
 	UserId        int64     `gorm:"column:user_id"`
 	ModelWeight   float64   `gorm:"column:model_weight"`
 	LengthOfMonth float64   `gorm:"column:length_of_month"`
-	CreatedDate   time.Time `gorm:"column:created_date"`
+	CreatedDate   time.Time `gorm:"column:created_date;type:datetime;autoCreateTime"`
 }
 
 func (Model) TableName() string { return "Model_Data" }
